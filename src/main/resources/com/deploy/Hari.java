f3837dbeb1e760888fe1942fef52c7e0d71f345d
vishwa1-art <maheshwaramshweta1@gmail.com>
2024-11-30 13:33:02 +0530

    Initial commit

---
+++ src/main/resources/com/deploy/Hari.java
@@ -0,0 +1,11 @@
+package com.deploy;
+
+public class Hari {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("deploy in progress");
+System.out.println("done");
+	}
+
+}
